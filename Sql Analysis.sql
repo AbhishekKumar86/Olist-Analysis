7f3c740f4a189470c2c91917a72e95888dd44802
Abhishek Kumar <akkisingh4784@gmail.com>
2025-06-30 14:10:39 +0530

    Update Sql Analysis.sql

--- Sql Analysis.sql
+++ Sql Analysis.sql
@@ -3,3 +3,17 @@ alter table order_review
 add constraint fk_order_id_rev
 foreign key (order_id) references orders(order_id)
 
+alter table order_items
+add constraint fk_order_id_item
+foreign key  (order_id) references orders(order_id)
+
+alter table Customer
+add constraint pk_csutomer_id_customer primary key (customer_id)
+
+alter table orders
+add constraint fk_customer_id_orders
+foreign key  (customer_id) references Customer(customer_id)
+
+alter table products
+add constraint pk_product_id_prducts
+primary key (product_id)
